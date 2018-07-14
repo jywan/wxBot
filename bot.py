016f20ad6faa94b80cd18b923c706ca8b276e320
wjy <jywan@outlook.com>
2018-07-14 21:55:34 +0800

    图灵机器人改为v2接口

--- bot.py
+++ bot.py
@@ -23,54 +23,72 @@ class TulingWXBot(WXBot):
 
     def tuling_auto_reply(self, uid, msg):
         if self.tuling_key:
-            url = "http://www.tuling123.com/openapi/api"
+            url = "http://www.tuling123.com/openapi/api/v2"
             user_id = uid.replace('@', '')[:30]
-            body = {'key': self.tuling_key, 'info': msg.encode('utf8'), 'userid': user_id}
-            r = requests.post(url, data=body)
+            headers = {'Content-Type': 'application/json'}
+            body = {
+                'reqType': 0,
+                'perception': {
+                    'inputText': {
+                        'text': msg
+                    }
+                },
+                'userInfo': {
+                    'apiKey': self.tuling_key,
+                    'userId': user_id
+                }
+
+            }
+            r = requests.post(url, headers=headers, data=json.dumps(body))
             respond = json.loads(r.text)
             result = ''
-            if respond['code'] == 100000:
-                result = respond['text'].replace('<br>', '  ')
-                result = result.replace(u'\xa0', u' ')
-            elif respond['code'] == 200000:
-                result = respond['url']
-            elif respond['code'] == 302000:
-                for k in respond['list']:
-                    result = result + u"【" + k['source'] + u"】 " + \
-                             k['article'] + "\t" + k['detailurl'] + "\n"
-            else:
-                result = respond['text'].replace('<br>', '  ')
-                result = result.replace(u'\xa0', u' ')
+            # 暂时只处理文本和url
+            groupType = 0
+            for res in respond['results']:
+                if res['groupType'] == 0:
+                    if res['resultType'] == 'text':
+                        result = res['values']['text'].replace('<br>', '  ')
+                    if res['resultType'] == 'url':
+                        result = res['values']['url']
+                else:
+                    if groupType == 0:
+                        groupType = res['groupType']
+                    if res['groupType'] == groupType:
+                        if res['resultType'] == 'text':
+                            result = result + '\t' + res['values']['text'].replace('<br>', '  ')
+                        if res['resultType'] == 'url':
+                            result = result + '\t' + res['values']['url']
 
             print('    ROBOT:', result)
             return result
         else:
-            return u"知道啦"
+            return "知道啦"
 
     def auto_switch(self, msg):
         msg_data = msg['content']['data']
-        stop_cmd = [u'退下', u'走开', u'关闭', u'关掉', u'休息', u'滚开']
-        start_cmd = [u'出来', u'启动', u'工作']
+        stop_cmd = ['退下', '走开', '关闭', '关掉', '休息', '滚开']
+        start_cmd = ['出来', '启动', '工作']
         if self.robot_switch:
-            for i in stop_cmd:
-                if i == msg_data:
-                    self.robot_switch = False
-                    self.send_msg_by_uid(u'[Robot]' + u'机器人已关闭！', msg['to_user_id'])
+            if msg_data in stop_cmd:
+                self.robot_switch = False
+                self.send_msg_by_uid('[Robot]' + '机器人已关闭！', msg['to_user_id'])
         else:
-            for i in start_cmd:
-                if i == msg_data:
-                    self.robot_switch = True
-                    self.send_msg_by_uid(u'[Robot]' + u'机器人已开启！', msg['to_user_id'])
+            if msg_data in start_cmd:
+                self.robot_switch = True
+                self.send_msg_by_uid('[Robot]' + '机器人已开启！', msg['to_user_id'])
 
     def handle_msg_all(self, msg):
         if not self.robot_switch and msg['msg_type_id'] != 1:
             return
-        if msg['msg_type_id'] == 1 and msg['content']['type'] == 0:  # reply to self
+        if msg['msg_type_id'] == 1 and msg['content']['type'] == 0:  # 自己发送的文本信息，发送命令来开启或关闭机器人
             self.auto_switch(msg)
         elif msg['msg_type_id'] == 4 and msg['content']['type'] == 0:  # text message from contact
             self.send_msg_by_uid(self.tuling_auto_reply(msg['user']['id'], msg['content']['data']), msg['user']['id'])
         elif msg['msg_type_id'] == 3 and msg['content']['type'] == 0:  # group text message
             if 'detail' in msg['content']:
+                '''detail元素类型为含有 type 与 value 字段的字典，
+                 type 为字符串 str (表示元素为普通字符串，此时value为消息内容) 
+                 或 at (表示元素为@信息， 此时value为所@的用户名)'''
                 my_names = self.get_group_member_name(msg['user']['id'], self.my_account['UserName'])
                 if my_names is None:
                     my_names = {}
@@ -86,13 +104,13 @@ class TulingWXBot(WXBot):
                             if my_names[k] and my_names[k] == detail['value']:
                                 is_at_me = True
                                 break
-                if is_at_me:
+                if is_at_me:  # 如果@了我
                     src_name = msg['content']['user']['name']
                     reply = 'to ' + src_name + ': '
                     if msg['content']['type'] == 0:  # text message
                         reply += self.tuling_auto_reply(msg['content']['user']['id'], msg['content']['desc'])
                     else:
-                        reply += u"对不起，只认字，其他杂七杂八的我都不认识，,,Ծ‸Ծ,,"
+                        reply += "对不起，只认字，其他杂七杂八的我都不认识，,,Ծ‸Ծ,,"
                     self.send_msg_by_uid(reply, msg['user']['id'])
 
 
